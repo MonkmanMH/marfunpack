1284b6b91caf480023781f661bf4f71c542cd0b1
Martin Monkman <monkmanmh@yahoo.ca>
2022-09-30 13:51:54 -0700

    create pop stats

--- R/mutating.R
+++ R/mutating.R
@@ -1,5 +1,7 @@
 # functions to add variables (could also be using in a `summarise()`)
 
+# NOTE: these are designed to work with StatCan data downloaded using {cansim} and cleanded with `janitor::clean_names()`
+
 # get_mom_stats / get_yoy_stats
 #
 # intended for monthly data series, calculates the month-over-month
@@ -21,5 +23,14 @@ get_yoy_stats <- function(df) {
            yoy_chg = (value - lag(value, n = 12)))
 }
 
+
+# get period over period (row over row?)
+get_pop_stats <- function(df) {
+  df %>%
+    arrange(ref_date) %>%
+    mutate(pop_val = lag(value), 
+           pop_pct = ((value / lag(value, n = 1)) - 1) * 100,
+           pop_chg = (value - lag(value, n = 1)))
+}
 
 
